0db391dad70715704a4084fc28bf710c528754ca
EC2 Default User <ec2-user@ip-172-31-21-104.ec2.internal>
2023-01-26 21:19:47 +0000

    Lec2

---
+++ Lec2.py
@@ -0,0 +1,36 @@
+"""
+lec 2
+numbers, strings, and variables
+"""
+
+
+
+#print('hello')
+#single line comment
+
+#print(type('hello'))
+#print(type(123))
+#print(type(123.123))
+
+#print('hello')
+#print("hello")
+#print("it's out 2nd python class")
+
+#print('Hello World')
+#print('Hello World'.lower()) #will lowercase, upper does the opposite
+#print(' hello world '.strip()) #will remove the surrounding spaces
+
+#print('Hello World')
+#print("Hello World".split()) #default, split by spaces
+#print('Hello World'.split('o')) #split by "o" - removes the o
+
+#print ('hello' + 'world') #combines the two string
+
+#print(1/2)
+
+my_num = 123
+#print(my_num)
+#print(my_num + 1)
+
+my_num = 234
+print(my_num)
